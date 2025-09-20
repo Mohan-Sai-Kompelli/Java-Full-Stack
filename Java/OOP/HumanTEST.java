4a8b3f5edf4ab92ad9b5a71629b6e25fbd8aa2c3
Mohan Sai Kompelli <mohansaikompelli15@gmail.com>
2025-09-20 14:29:07 +0530

    Create HumanTEST.java

---
+++ Java/OOP/HumanTEST.java
@@ -0,0 +1,11 @@
+
+public class HumanTEST {
+
+	public static void main(String[] args) {
+		Human h = new Human();
+		h.setName("King");
+		h.setAge(22);
+		System.out.println("Name is : "+h.getName()+" The age is  : "+h.getAge());
+	}
+
+}
