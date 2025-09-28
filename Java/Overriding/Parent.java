4e47e34981a5b3f2fbbfd0b4947af4e78a361e9e
Mohan Sai Kompelli <mohansaikompelli15@gmail.com>
2025-09-28 21:59:13 +0530

    Create Parent.java

---
+++ Java/Overriding/Parent.java
@@ -0,0 +1,14 @@
+
+public class Parent {
+
+	
+	public void property() {
+		System.out.println("Cash/Gold/Land");
+	}
+	
+	
+	//overriden method
+	public void marry() {
+		System.out.println("Niharika");
+	}
+}
