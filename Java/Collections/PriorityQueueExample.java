8043436e4133c315e235fd482cd06abf20c3fa9b
Mohan Sai Kompelli <mohansaikompelli15@gmail.com>
2025-10-25 17:23:19 +0530

    Create PriorityQueueExample.java

---
+++ Java/Collections/PriorityQueueExample.java
@@ -0,0 +1,17 @@
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueExample {
+	
+	public static void  main(String[] args) {
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		pq.add(5);
+		pq.add(3);
+		pq.add(1);
+		pq.add(4);
+		pq.add(2);
+		pq.add(6);
+		System.out.println(pq);
+	}
+
+}
