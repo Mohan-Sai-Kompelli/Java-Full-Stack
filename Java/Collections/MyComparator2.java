513c32e58e684f5f210ace07310027ecd496efbc
Mohan Sai Kompelli <mohansaikompelli15@gmail.com>
2025-11-01 23:40:12 +0530

    Create MyComparator2.java

---
+++ Java/Collections/MyComparator2.java
@@ -0,0 +1,10 @@
+
+import java.util.Comparator;
+
+public class MyComparator2 implements Comparator {
+	public int compare(Object obj1, Object obj2) {
+		String s1 = obj1.toString();
+		String s2 = obj2.toString();
+		return -s1.compareTo(s2);
+	}
+}
