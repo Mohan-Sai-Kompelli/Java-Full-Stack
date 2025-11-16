7f5e29032f97f1192f69c6e223732a6cf68d7cf8
Mohan Sai Kompelli <mohansaikompelli15@gmail.com>
2025-11-16 20:04:47 +0530

    Create NumberPyramid.java

---
+++ Java/Practise/NumberPyramid.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class NumberPyramid {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		py(n);
+		
+	}
+	public static void py(int n) {
+		int cnt  =1;
+for(int i=1;i<=n;i++) {
+			
+			for(int j=0;j<=n-i;j++) {
+				System.out.print(" ");
+			}
+			
+			for(int j=1;j<=i;j++) {
+				System.out.print(cnt+" ");
+				cnt++;
+			}
+			
+			System.out.println();
+			
+			
+		}
+	}
+
+}
