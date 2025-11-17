1287e7f33afa65a1c04815d9045ecd2966e9bdb5
Mohan Sai Kompelli <mohansaikompelli15@gmail.com>
2025-11-17 20:34:04 +0530

    Create NumberTriangle.java

---
+++ Java/Practise/NumberTriangle.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class NumberTriangle {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		num(n);
+		
+}
+	public static void num(int n) {
+		for(int i=1;i<=n;i++) {
+			
+			for(int j=0;j<=n-i;j++) {
+				System.out.print(" ");
+			}
+			
+			for(int j=1;j<=i;j++) {
+				System.out.print(i);
+			}
+			
+			System.out.println();
+			
+		}
+	}
+
+}
