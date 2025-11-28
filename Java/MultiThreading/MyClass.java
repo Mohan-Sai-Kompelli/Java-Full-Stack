4d5f8564ff783ef9b1b9c8c46fceba2bfb8afd8a
Mohan Sai Kompelli <mohansaikompelli15@gmail.com>
2025-11-28 11:06:40 +0530

    Create MyClass.java

---
+++ Java/MultiThreading/MyClass.java
@@ -0,0 +1,15 @@
+
+public class MyClass implements Runnable{
+
+	@Override
+	public void run() {
+		for(int i=0;i<10;i++) {
+			Thread.yield();
+			System.out.println("Child Thread");
+		}
+		
+	}
+
+	
+	
+}
