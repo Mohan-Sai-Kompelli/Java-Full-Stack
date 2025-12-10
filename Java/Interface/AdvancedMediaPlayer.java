5049261b65d558fa614c507135e1c33b5ec9d94b
Mohan Sai Kompelli <mohansaikompelli15@gmail.com>
2025-12-10 22:28:42 +0530

    Create AdvancedMediaPlayer.java

---
+++ Java/Interface/AdvancedMediaPlayer.java
@@ -0,0 +1,11 @@
+
+public interface AdvancedMediaPlayer {
+	
+	
+	// method to play vlc format
+	void playvlc(String filename);
+	// method to play mp4 file format
+	void playmp4(String filename);
+	
+
+}
